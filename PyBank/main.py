6abf179f8b7f19bbf407d73225fb866474e5d604
Dmytro Dmytrenko <dmytrodmytrenko@Dmytros-MacBook-Pro.local>
2019-02-10 11:40:43 -0500

    final updates of homework files

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,15 +7,50 @@ with open(cereal_csv, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
     csv_header = next(csvfile)
-    # print(f"Header: {csv_header}")
+    print(f"Header: {csv_header}")
+    total_row = 0
+    total_sum = 0
+    prev_row = 0
+    total_delta = 0
+    greatest_increase = 0
+    greatest_increase_date = ''
+    greatest_decrease = 0
+    greatest_decrease_date = ''
+    first_value = 0
 
-    row_count = sum(1 for row in csvreader)
-    print(f"Total Months: {row_count}")
-
-    total = 0
-    #total = sum(row[1])
     for row in csvreader:
-        print(row)
-        for col in row[1]:
-            total += int(col)
-    print(f'Total: {total}')
+        total_row += 1
+        total_sum += int(row[1])
+        row.append('delta')
+        row[2] = int(row[1]) - prev_row
+        total_delta += int(row[2])
+        prev_row = int(row[1])
+        if csvreader.line_num == 1:
+            first_value = int(row[1])
+
+        if int(row[2]) > greatest_increase:
+            greatest_increase = int(row[2])
+            greatest_increase_date = str(row[0])
+               
+        if int(row[2]) < greatest_decrease:
+            greatest_decrease = int(row[2])
+            greatest_decrease_date = str(row[0])
+    
+    print("Financial Analysis")
+    print("------------------------")
+    print(f'Total Months: {total_row}')
+    print(f'Total: {total_sum}')
+    print(f'Average Change: {round((total_delta-first_value)/total_row,2)}')
+    print(f'Greates Increase in Profits: {greatest_increase_date} ({greatest_increase})')
+    print(f'Greates Decrease in Profits: {greatest_decrease_date} ({greatest_decrease})')
+
+with open("results.txt", "w") as file:
+    file.write(f' Financial Analysis \n \
+--------------------\n \
+Total: {total_row} \n \
+Total: {total_sum} \n \
+Average Change: {round((total_delta-first_value)/total_row,2)} \n \
+Greates Increase in Profits: {greatest_increase_date} ({greatest_increase}) \n \
+Greates Decrease in Profits: {greatest_decrease_date} ({greatest_decrease})')
+    file.close()
+    
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,8 +7,52 @@ with open(election_csv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csvheader = next(csvfile)
     
+    total_row = 0
+    khan = 0
+    correy = 0
+    li = 0
+    tooley = 0
+
     print("Election Results")
     print("---------------------")
-    row_count = sum(1 for row in csvreader)
-    print(f"Total Votes: {row_count}")
+    for row in csvreader:
+        total_row += 1
+        if row[2] == 'Khan':
+            khan += 1
+        elif row[2] == 'Correy':
+            correy += 1
+        elif row[2] == 'Li':
+            li += 1
+        elif row[2] == "O'Tooley":
+            tooley += 1
+    
+    if max(khan, correy, li, tooley) == khan:
+        winner = 'Khan'
+    elif max(khan, correy, li, tooley) == correy:
+        winner = 'Correy'
+    elif max(khan, correy, li, tooley) == li:
+        winner = 'Li'
+    elif max(khan, correy, li, tooley) == tooley:
+        winner = "O'Tooley"
+
+    print(f"Total Votes: {total_row}")
     print("---------------------")
+    print(f"Khan: {round((khan/total_row)*100,3)}% {khan}")
+    print(f"Correy: {round((correy/total_row)*100,3)}% {correy}")
+    print(f"Li: {round((li/total_row)*100,3)}% {li}")
+    print(f"O'Tooley: {round((tooley/total_row)*100,3)}% {tooley}")
+    print("---------------------")
+    print(f"Winner: {winner}")
+    print("---------------------")
+
+with open("election_results.txt", "w") as file:
+    file.write(f"Total Votes: {total_row} \n \
+--------------------\n \
+Khan: {round((khan/total_row)*100,3)}% {khan} \n \
+Correy: {round((correy/total_row)*100,3)}% {correy} \n \
+Li: {round((li/total_row)*100,3)}% {li} \n \
+O'Tooley: {round((tooley/total_row)*100,3)}% {tooley} \n \
+--------------------\n \
+Winner: {winner} \n \
+--------------------")
+    file.close()
